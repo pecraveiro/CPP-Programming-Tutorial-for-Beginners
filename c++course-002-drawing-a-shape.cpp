94e348b302bf052f711b90501c0394f9e8ad4f1b
pecraveiro <pecraveiro1@gmail.com>
2021-11-11 19:37:15 -0300

    O segundo arquivo do curos, explicando o código e desenhando uma forma.

---
+++ c++course-002-drawing-a-shape.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    cout << "      /|" << endl;
+    cout << "     /_|" << endl;
+    cout << "    /__|" << endl;
+    cout << "   /___|" << endl;
+    cout << "  /____|" << endl;
+    cout << " /_____|" << endl;
+    cout << "/______|" << endl;
+
+    return 0;
+}
+
+/* Configuração básica do código
+
+#include <iostream> 
+
+using namespace std;
+
+*/
+
+/* Function / Função: 
+- container para colocar os códigos / container to us to put the code inside of our c++ programms.
+
+int main()
+
+*/
+
+// cout = Console OUT
+// endl = end LINE
