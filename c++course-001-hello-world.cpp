c63b3228fe5b8e9112cf53de1e46ea9e4c7bdd6a
pecraveiro <pecraveiro1@gmail.com>
2021-11-11 19:36:19 -0300

    O primeiro arquivo do curso, introdução e o primeiro código.

---
+++ c++course-001-hello-world.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+// Anotações do vídeo
+/* 
+
+"This is gonna print something out for us'' - Isso vai retornar uma mensagem 
+
+Instruções para o computador:
+- Build the programm (Build the file);
+- Run the programm (Run the file).
+
+- Construir o programa (escrever o código);
+- Rodar o programa;
+
+*/
