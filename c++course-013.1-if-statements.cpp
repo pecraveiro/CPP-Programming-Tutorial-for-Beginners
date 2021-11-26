c0905becfa7301ab191d832921d620068076588e
pecraveiro <pecraveiro1@gmail.com>
2021-11-26 01:35:48 -0300

    If statements

---
+++ c++course-013.1-if-statements.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    bool isMale = true;
+
+    if(isMale) // Vamos especificar uma condição
+    {   
+        cout << "You're a male.";
+    }
+
+    else
+    { 
+        "You aren't a male, you're a female.";
+    }
+
+    return 0;
+}
---
+++ c++course-013.2-if-statements.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    bool isMale = true;
+    bool isTall = true;
+
+    if(isMale && isTall) // Vamos especificar uma condição - && and operator
+    {   
+        cout << "You're a tall male.";
+    }
+
+    else
+    { 
+        "You aren't a male, you're a female and aren't tall.";
+    }
+
+    return 0;
+}
