364c3c3d47b9924db2f6e1fac5ef2cc2b007e7f9
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-01-01 11:55:15 -0300

    While Loop

--- c++course-017.1-loop-while.cpp
+++ c++course-017.1-loop-while.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-	
+	// While Loop
 	int index = 1;
 	while(index <= 5) { // Especificando uma condição para o While
 		cout << index << endl; // Vamos printar o index
