543de0c23390d7cf2260958d26749298523f199a
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-01-01 11:45:41 -0300

    Create c++course-017.1-loop-while.cpp

---
+++ c++course-017.1-loop-while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	int index = 1;
+	while(index <= 5) { // Especificando uma condição para o While
+		cout << index << endl; // Vamos printar o index
+		index++; // ++ - Incremento
+	}
+	
+return 0;
+}
