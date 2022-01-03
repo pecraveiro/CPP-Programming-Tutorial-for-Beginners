b63a388353ae765b478c8c2ebec0c96ab99c46cf
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-01-03 18:57:30 -0300

    Building a Guessing Game

---
+++ c++course-018-loop-while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{	
+	// Declaração de variáveis
+	int secretNum = 7;
+	int guess;
+
+	while (secretNum != guess){
+		
+	cout << "Enter guess: ";
+	cin >> guess;
+	}
+
+	cout << "You Win!";
+}
