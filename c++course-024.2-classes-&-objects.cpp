2e6d4bc81002fa89bd14dac92e21622f4f7e3535
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-01-05 13:07:24 -0300

    Extra - Classes e Objetos - Meus Livros

--- c++course-024.2-classes-&-objects.cpp
+++ c++course-024.2-classes-&-objects.cpp
@@ -4,10 +4,10 @@ using namespace std;
 // Class - Template para Book
 class Book {
     public:
-    
+
     // Especificar o que a classe Book terá
     // Blue print for the book data type
-        
+
         string title;
         string author;
         int pages;
@@ -21,30 +21,30 @@ int main()
     book1.title = "Warcraft - A Trilogia da Fonte do Sol";
     book1.author = "Richard A. Knaak & Jae-Hwan Kim";
     book1.pages = 130;
-    
+
     cout << "Nome do livro: "<< book1.title << endl;
     cout << "Autor: " << book1.author << endl;
     cout << "Páginas: " << book1.pages << endl << endl;
-    
+
     Book book2; // Segundo livro
-    
+
     book2.title = "World of Warcraft - Death Knight";
     book2.author = "Dan Jolley";
     book2.pages = 150;
-    
+
     cout << "Nome do livro: "<< book2.title << endl;
     cout << "Autor: " << book2.author << endl;
-    cout << "Páginas: " << book2.pages << endl;
-    
+    cout << "Páginas: " << book2.pages << endl << endl;
+
     Book book3; // Terceiro livro
-  
+
     book3.title = "Warcraft - Lendas - Volume 1";
     book3.author = "Vários Autores";
     book3.pages = 180;
-    
+
     cout << "Nome do livro: "<< book3.title << endl;
     cout << "Autor: " << book3.author << endl;
     cout << "Páginas: " << book3.pages << endl;
-    
+
     return 0;
 }
