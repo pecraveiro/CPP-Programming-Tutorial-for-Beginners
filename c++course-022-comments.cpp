bd588c523f1efdbb2cd821d4dd2226e69e39e704
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-01-05 11:14:07 -0300

    Como fazer um comentário em seu código
    Acredito que essa aula do curso deveria ter sido passada no começo dele, para melhor organização das anotações.

---
+++ c++course-022-comments.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Comments are fun..."; 
+  // Lorem Ipsum
+  
+  /*
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+  Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. 
+  Voluptatum ducimus voluptates voluptas?
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+  Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!
+  */
+}
