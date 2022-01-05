73f2bab1f57b9b01090e5f5d0052e45380ec1b16
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-01-04 22:53:36 -0300

    Array with For Loops

---
+++ c++course-019.2-for-loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// For Loops - Array
+
+int main()
+{	
+    int nums[] = {1, 2, 5, 7, 3, 22, 55, 69, 420};
+  
+    for(int i = 0; i <= 8; i++) {
+        
+        cout << nums[i] << endl;
+        
+    }
+	
+	return 0;
+}
