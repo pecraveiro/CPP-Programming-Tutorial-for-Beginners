a977aea91ac89bca982492ddacb56999988d3586
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-01-04 23:04:31 -0300

    Função Exponencial
    Tive um pouco de dificuldade ao refazer o código, preciso revisar essa aula

---
+++ c++course-020-exponent-function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Função Exponencial
+
+int power(int baseNum, int powNum) {
+  
+    int result = 1;
+    
+    for(int i = 0; i < powNum; i++) {
+        result = result * baseNum;
+    }
+        
+      
+    return result;
+}
+
+int main()
+{
+  
+  cout << power(2, 3); // X elevado a Y, nesse caso 2 ^ 3 = 8
+  
+  return 0;
+}
